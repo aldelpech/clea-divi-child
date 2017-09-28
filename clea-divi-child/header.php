d10f7f4463142af8a481316e36967276778c531e
Anne-Laure Delpech <contact@parcours-performance.com>
2017-09-28 14:40:39 +0200

    adjust css of breadcrumb

--- clea-divi-child/header.php
+++ clea-divi-child/header.php
@@ -283,8 +283,9 @@
 		</header> <!-- #main-header -->
 
 		<div id="et-main-area">
-<?php if(function_exists('yoast_breadcrumb') && !is_front_page()) {
- echo '<div class="container">';
- yoast_breadcrumb('<p id="breadcrumbs">','</p>');
- echo '</div>';
-} ?>
+			<!-- #main-header -->
+			<?php if(function_exists('yoast_breadcrumb') && !is_front_page()) {
+				echo '<div class="container">';
+				yoast_breadcrumb('<p id="breadcrumbs">','</p>');
+				echo '</div>';
+			} ?>
