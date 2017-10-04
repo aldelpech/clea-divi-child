95e5a6df4a772adcb699136bdba3106981c8606e
Anne-Laure Delpech <contact@parcours-performance.com>
2017-10-04 15:54:38 +0200

    shortcode for category title and description display

--- clea-divi-child/functions.php
+++ clea-divi-child/functions.php
@@ -1,5 +1,17 @@
 <?php
 /**
+ * @package    clea-divi-child
+ * @subpackage Functions
+ * @version    1.0
+ * @since      0.1.0
+ * @author     Anne-Laure Delpech <ald.kerity@gmail.com>  
+ * @copyright  Copyright (c) 2017 7Anne-Laure Delpech
+ * @link       http://knowledge.parcours-performance.com
+ * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
+
+/**
  * @author Divi Space
  * @copyright 2017
  */
@@ -19,7 +31,29 @@ add_action( 'wp_enqueue_scripts', 'ds_ct_loadjs' );
 include('login-editor.php');
 
 
+/******
+* Code added by AL (CLEA) to the original child theme
+*/
 
+// To require a file which is somewhere in this child theme directory
+// require_once( trailingslashit( get_stylesheet_directory() ) . 'widgets.php' );
+
+/***** a shortcode to add category title and description ***********/
+function cdc_shortcode($atts = [], $content = null)
+{
+
+	// Display category title and description
+	$content = '<header class="archive-header">' ;
+	$content .= '<h1 class="entry-title main_title">' . single_cat_title( '', false ) . "</h1>" ;
+	$content .= '<div class="archive-meta">' . category_description() . "</div></header>" ;
+
+    // always return
+    return $content;
+}
+add_shortcode('display_cat_title_description', 'cdc_shortcode');
+
+
+// A titre d'essai - ne fonctionne pas... 
 function custom_colors_css() {
 	
 	global $themename, $shortname, $options;
